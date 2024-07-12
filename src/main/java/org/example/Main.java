78e793fc5413ea97e73b6ef10260de27276eb4a4
Bajahaw <rathi7137@gmail.com>
2024-07-13 00:05:59 +0200

    initial first push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("this project is for learning purposes");
+        //TODO: implement the game
     }
 }
